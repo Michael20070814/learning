73883346df6e6da24806be7924e0b05d91e22e81
Yang <Michael20070814@outlook.com>
2025-10-11 20:11:49 +0800

    learning some compute

---
+++ chapter_5/golf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+    int jane, tarzan, cheeta;
+
+    cheeta = tarzan = cheeta = 68;
+    printf("                  cheeta   tarzan    jane\n");
+    printf("First round score %4d %8d %8d\n", cheeta, tarzan, jane);
+
+    return 0;
+}
